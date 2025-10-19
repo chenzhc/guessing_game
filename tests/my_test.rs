1102fc96d328319ae51ec2b77134455bd6b05f2c
chenzhichao <80139283@qq.com>
2025-10-19 21:05:05 +0800

    add

--- tests/my_test.rs
+++ tests/my_test.rs
@@ -5,7 +5,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use std::{collections::HashMap, path::Ancestors};
+use std::{collections::HashMap, fs::File, io::{self, ErrorKind, Read}, path::Ancestors};
 
 use guessing_game::init;
 use log::info;
@@ -324,6 +324,51 @@ fn it_vector_test01() {
     }
     info!("{:?}", map);
 
+}
+
+#[test]
+fn it_result_test01() {
+    init();
+
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+}
+
+fn read_username_from_file3() -> anyhow::Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file2() -> anyhow::Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
 
 fn add_fancy_hat() {
