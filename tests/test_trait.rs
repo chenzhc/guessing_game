00b48190470b8185ac1f905da5900fd6f29e4443
chenzhichao <80139283@qq.com>
2025-10-20 21:41:52 +0800

    add

--- tests/test_trait.rs
+++ tests/test_trait.rs
@@ -6,7 +6,7 @@
 #![allow(unused_variables)]
 
 
-use std::fmt::{format, Display};
+use std::{fmt::{format, Display}, thread, time::Duration};
 
 use guessing_game::init;
 use log::info;
@@ -92,6 +92,76 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     } else {
         return y;
     }
+}
+
+fn simulated_expendsive_calculation(intensity: u32) -> u32 {
+    info!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+fn genrate_workout(intensity: u32, random_number: u32) {
+    // let expensive_result = simulated_expendsive_calculation(intensity);
+
+    let mut expensive_closure = Cacher::new(|num| {
+        info!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        info!("Today, do {} pushups!", expensive_closure.value(intensity));
+        info!("Next, do {} situps!", expensive_closure.value(intensity));
+
+    } else {
+        if random_number == 3 {
+            info!("Take a break today! Remember to stay hydrated!");
+        } else {
+            info!(
+                "Today, run fo {} minutes!",
+                expensive_closure.value(intensity)
+            );
+        }
+    }
+}
+
+struct Cacher<T>  
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T> 
+    where T: Fn(u32) -> u32 
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher { calculation: calculation, value: None }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
+#[test]
+fn it_generate_test01() {
+    init();
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    genrate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+
 }
 
 #[test]
