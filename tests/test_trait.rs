4164474c849e429799fba1e4e7854d931bea34b7
chenzc <80132938@qq.com>
2025-10-21 14:39:11 +0800

    add

--- tests/test_trait.rs
+++ tests/test_trait.rs
@@ -151,6 +151,101 @@ impl<T> Cacher<T>
     }
 }
 
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == shoe_size)
+        .collect()
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 { 
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn it_calling_next_directly_test01() {
+    init();
+    let mut counter = Counter::new();
+    // info!("{:?}", counter.next());
+
+    for next in counter.into_iter() {
+        info!("{:?}", next);
+    }
+
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    info!("sum: {}", sum);
+}
+
+#[test]
+fn it_filters_by_size_test01() {
+    init();
+
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker")},
+        Shoe { size: 13, style: String::from("sandal")},
+        Shoe { size: 10, style: String::from("boot")},
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+    info!("{:?}", in_my_size);
+
+}
+
+#[test]
+fn it_iter_test01() {
+    init();
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
+    info!("{:?}", v1);
+    
+    for val in v1_iter {
+        info!("Got: {}", val);
+    }
+
+    let v1 = vec![1,2,3];
+    let mut v1_iter = v1.iter();
+    info!("{:?}", v1_iter.next());
+
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
+    let total: i32 = v1_iter.sum();
+    info!("{}", total);
+
+    let v1 = vec![1,2,3];
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    info!("{:?}", v2);
+
+}
+
 #[test]
 fn it_close_test01() {
     init();
