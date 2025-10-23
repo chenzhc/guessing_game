0453df36404abd3669b6a7e2413e38084b182348
chenzc <80132938@qq.com>
2025-10-23 17:08:39 +0800

    add

---
+++ tests/my_weak.rs
@@ -0,0 +1,42 @@
+#![cfg_attr(
+    debug_assertions,
+    allow(dead_code, unused_imports, unused_variables, unused_mut)
+)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::{cell::RefCell, rc::{Rc, Weak}};
+use guessing_game::init;
+use log::info;
+
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+#[test]
+fn it_weak_test01() {
+    init();
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    info!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    
+    info!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+}
+
