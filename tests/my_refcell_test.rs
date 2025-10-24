d8d5750e4066024d35524762072e0e3410cc09c5
chenzhichao <80139283@qq.com>
2025-10-24 21:31:21 +0800

    add

--- tests/my_refcell_test.rs
+++ tests/my_refcell_test.rs
@@ -5,7 +5,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use std::{cell::{Ref, RefCell}, ops::Deref, rc::Rc};
+use std::{cell::{Ref, RefCell}, ops::Deref, rc::{Rc, Weak}};
 use guessing_game::init;
 use log::info;
 use List::{Cons, Nil};
@@ -30,7 +30,31 @@ impl List {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
+}
+
+#[test]
+fn it_weak_test02() {
+    init();
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    info!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    info!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
 }
 
 #[test]
@@ -39,11 +63,13 @@ fn it_weak_test01() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
